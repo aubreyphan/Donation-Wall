9a73b86480821c6e46ccf7c240dcd12dde5771c9
Aubrey Phan <nhakhanhphan@gmail.com>
2017-06-13 14:10:41 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,50 @@
+$(function(){
+	
+	//odometer
+	window.odometerOptions={
+		format: '(,ddd).dd',
+		theme: 'train-station',
+	};
+
+	  setTimeout(function(){
+	    $('.odometer').html(2000000);
+	  }, 1000/2);
+
+	//grid of each contribution post
+	$('.grid').masonry({
+		itemSelector: '.item',
+		columnWidth: 200,
+		gutter: 10
+	});
+
+	$('.item').each(function(i,j){
+		var animationList = ['bounceIn', 'fadeInDown', 'fadeInUp', 'flipInX', 'flipInY', 'rotateIn', 'slideInDown', 'zoomIn'];
+		var j=Math.floor(Math.random()*8);
+
+		setTimeout(function(){
+			$('.item').eq(i).addClass('is-visible animated ' + animationList[j]);
+		}, 200*i);
+
+	});
+	
+	//display Date and Time at footer
+	var d = new Date();
+	var formatted_d = format_time(d);
+	$('#dateTime').text(formatted_d);
+
+	function format_time(t){
+		hours = format_2_digits(t.getHours());
+		minutes = format_2_digits(t.getMinutes());
+		return hours + ':' + minutes;
+	};
+
+	function format_2_digits(t){
+		return t < 10 ? '0'+t : t;
+	};
+
+	//alert transition
+	$('.grid .item').click(function(){
+		$('.alert-element').toggleClass('is-active');
+	});
+	
+});
